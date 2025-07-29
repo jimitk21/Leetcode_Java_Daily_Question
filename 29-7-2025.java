3a3b5027a85e9246d87f23106d321fc084adb0f2
Jimit Karangia <jimitk21@gmail.com>
2025-07-29 22:08:18 +0530

    29 jul code added

---
+++ 29-7-2025.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public int[] smallestSubarrays(int[] nums) {
+
+        int length = nums.length;
+
+        int[] answer = new int[length];
+
+        int[] latestOneBitIndices = new int[32];
+
+        Arrays.fill(latestOneBitIndices, -1);
+
+        for (int i = length - 1; i >= 0; --i) {
+
+            int subarraySize = 1;
+
+            for (int j = 0; j < 32; ++j) {
+
+                if (((nums[i] >> j) & 1) == 1) {
+
+                    latestOneBitIndices[j] = i;
+
+                } else if (latestOneBitIndices[j] != -1) {
+
+                    subarraySize = Math.max(subarraySize, latestOneBitIndices[j] - i + 1);
+
+                }
+
+            }
+
+            answer[i] = subarraySize;
+
+        }
+
+        return answer;
+
+    }
+
+}
